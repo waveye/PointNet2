2ddee137005e1fc34e0f528c9c5c4a204c302506
Julian Klein <julian@waveye.com>
2025-05-29 19:06:33 +0200

    Hyperparam adapted.

--- pointnet2/models/utils.py
+++ pointnet2/models/utils.py
@@ -173,7 +173,7 @@ class PointNetSetAbstraction(nn.Module):
         last_channel = in_channel
         for out_channel in mlp:
             self.mlp_convs.append(nn.Conv2d(last_channel, out_channel, 1))
-            self.mlp_bns.append(nn.BatchNorm2d(out_channel))
+            self.mlp_bns.append(nn.BatchNorm2d(out_channel, momentum=0.01))
             last_channel = out_channel
         self.out_channel = last_channel
         self.group_all = group_all
