b852d9e3f9c116ac6a71c5457f104f650a130123
Julian Klein <julian@waveye.com>
2025-06-01 12:49:55 +0200

    Config

--- pointnet2/models/cls_ssg.py
+++ pointnet2/models/cls_ssg.py
@@ -16,19 +16,19 @@ class get_model(nn.Module):
 
         self.sa1 = PointNetSetAbstraction(
             npoint=60, radius=0.05, nsample=15,
-            in_channel=self.transform.num_dimensions_transformed, mlp=(32, 32, 64))
+            in_channel=self.transform.num_dimensions_transformed, mlp=(8, 16, 16))
         self.sa2 = PointNetSetAbstraction(
             npoint=30, radius=0.1, nsample=20,
-            in_channel=3 + self.sa1.out_channel, mlp=(32, 32, 64))
+            in_channel=3 + self.sa1.out_channel, mlp=(16, 16, 32))
         self.sa3 = PointNetSetAbstraction(
-            in_channel=3 + self.sa2.out_channel, mlp=(128, 256, 512), group_all=True)
+            in_channel=3 + self.sa2.out_channel, mlp=(64, 128, 256), group_all=True)
         self.fc1 = nn.Linear(self.sa3.out_channel, 128)
         self.bn1 = nn.BatchNorm1d(128,
                                   momentum=0.1)
-        self.drop1 = nn.Dropout(0.2)
+        self.drop1 = nn.Dropout(0.4)
         self.fc2 = nn.Linear(128, 64)  # Aligned with tf_pipeline -> Reduced from 576 to 256
         self.bn2 = nn.BatchNorm1d(64, momentum=0.1)
-        self.drop2 = nn.Dropout(0.2)
+        self.drop2 = nn.Dropout(0.4)
         self.fc3 = nn.Linear(64, num_classes)  # Aligned with tf_pipeline -> Reduced from 160 to 128
 
     def forward(self, data, mask=None):
