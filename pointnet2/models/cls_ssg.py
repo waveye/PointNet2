a29adc6b6879ccb77250be18df9d85e99ccf7573
Julian Klein <julian@waveye.com>
2025-06-01 11:29:47 +0200

    Extremely shrinked.

--- pointnet2/models/cls_ssg.py
+++ pointnet2/models/cls_ssg.py
@@ -29,7 +29,7 @@ class get_model(nn.Module):
         self.fc2 = nn.Linear(128, 64)  # Aligned with tf_pipeline -> Reduced from 576 to 256
         self.bn2 = nn.BatchNorm1d(64, momentum=0.1)
         self.drop2 = nn.Dropout(0.2)
-        self.fc3 = nn.Linear(64, num_classes)  # Aligned with tf_pipeline -> Reduced from 160 to 128
+        self.fc3 = nn.Linear(128, num_classes)  # Aligned with tf_pipeline -> Reduced from 160 to 128
 
     def forward(self, data, mask=None):
         B, N, D = data.shape
@@ -41,7 +41,7 @@ class get_model(nn.Module):
         l3_xyz, l3_points = self.sa3(l2_xyz, l2_points)
         x = l3_points.view(B, 512)
         x = self.drop1(F.relu(self.bn1(self.fc1(x))))
-        x = self.drop2(F.relu(self.bn2(self.fc2(x))))
+        # x = self.drop2(F.relu(self.bn2(self.fc2(x))))
         x = self.fc3(x)
         x = F.log_softmax(x, -1)
 
