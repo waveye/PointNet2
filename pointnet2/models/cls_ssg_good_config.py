ee18c6e29cc68666ef8f9c86786d15ec6cefcfcb
Julian Klein <julian@waveye.com>
2025-06-02 11:04:09 +0200

    Now config.

--- pointnet2/models/cls_ssg_good_config.py
+++ pointnet2/models/cls_ssg_good_config.py
@@ -15,10 +15,10 @@ class get_model(nn.Module):
         self.unit_cube_normalization = UnitCubeNormalization(eps=1e-6)
 
         self.sa1 = PointNetSetAbstraction(
-            npoint=100, radius=0.1, nsample=30,
+            npoint=100, radius=0.2, nsample=20,
             in_channel=self.transform.num_dimensions_transformed, mlp=(64, 64, 128))
         self.sa2 = PointNetSetAbstraction(
-            npoint=50, radius=0.2, nsample=20,
+            npoint=50, radius=0.4, nsample=40,
             in_channel=3 + self.sa1.out_channel, mlp=(64, 64, 128))
         self.sa3 = PointNetSetAbstraction(
             in_channel=3 + self.sa2.out_channel, mlp=(256, 512, 1024), group_all=True)
