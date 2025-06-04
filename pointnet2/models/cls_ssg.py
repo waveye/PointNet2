2234933e992f9c8f33f28a781ac4acaae8165eda
Julian Klein <julian@waveye.com>
2025-06-04 19:43:25 +0200

    mod parameters

--- pointnet2/models/cls_ssg.py
+++ pointnet2/models/cls_ssg.py
@@ -18,7 +18,7 @@ class get_model(nn.Module):
         self.npoint1 = 50
         self.npoint2 = 30
         self.nsample1 = 10
-        self.nsample2 = 20
+        self.nsample2 = 10
         self.mlp1 = (64, 64, 128)
         self.mlp2 = (64, 64, 128)
         self.mlp3 = (128, 256, 512)
