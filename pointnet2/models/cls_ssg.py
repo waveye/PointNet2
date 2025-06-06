95b7feea2c6a54e8ef519ac8c6910fe42fe046e0
Julian Klein <julian@waveye.com>
2025-06-06 15:48:21 +0200

    Added comments

--- pointnet2/models/cls_ssg.py
+++ pointnet2/models/cls_ssg.py
@@ -14,8 +14,8 @@ class get_model(nn.Module):
 
         # Config
         self.absolute_radius = True
-        # If absolute_radius=False: r1 / r2 are fractions of 2*max_std_variance (max of std_x, std_y, std_z) of a sample
-        # If absolute_radius=True: r1 / r2 are absolute radii for ball query
+        # If absolute_radius=False: r1, ... are fractions of 2*max_std_variance (max of std_x, std_y, std_z) of a sample
+        # If absolute_radius=True: r1, ... are absolute radii for ball query
         self.r1 = 0.25  # fine features
         self.r2 = 0.5   # local features
         self.r3 = 0.75
