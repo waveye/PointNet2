52a834dd2fce5c2b9c6b7b1d27313ef28a42af01
Julian Klein <julian@waveye.com>
2025-06-06 11:22:53 +0200

    Expanded model

--- pointnet2/models/cls_ssg.py
+++ pointnet2/models/cls_ssg.py
@@ -16,15 +16,27 @@ class get_model(nn.Module):
         self.absolute_radius = True
         # If absolute_radius=False: r1 / r2 are fractions of 2*max_std_variance (max of std_x, std_y, std_z) of a sample
         # If absolute_radius=True: r1 / r2 are absolute radii for ball query
-        self.r1 = 0.2
-        self.r2 = 0.3
-        self.npoint1 = 50
-        self.npoint2 = 30
+        self.r1 = 0.1
+        self.r2 = 0.2
+        self.r3 = 0.4
+        self.r4 = 0.6
+
+        self.npoint1 = 100
+        self.npoint2 = 80
+        self.npoint3 = 60
+        self.npoint4 = 20
+
         self.nsample1 = 10
-        self.nsample2 = 20
-        self.mlp1 = (32, 32, 64)
-        self.mlp2 = (64, 64, 128)
-        self.mlp3 = (128, 256, 512)
+        self.nsample2 = 10
+        self.nsample3 = 10
+        self.nsample4 = 10
+
+        self.mlp1 = (16, 16, 32)
+        self.mlp2 = (32, 32, 32)
+        self.mlp3 = (32, 32, 64)
+        self.mlp4 = (64, 64, 128)
+        self.mlp_last = (128, 256, 512)
+
         self.fc1_out = 128
         self.fc2_out = 64
         self.fc_dropout = 0.3
@@ -36,8 +48,14 @@ class get_model(nn.Module):
             npoint=self.npoint2, radius=self.r2, nsample=self.nsample2,
             in_channel=3 + self.sa1.out_channel, mlp=self.mlp2, radius_absolute=self.absolute_radius)
         self.sa3 = PointNetSetAbstraction(
-            in_channel=3 + self.sa2.out_channel, mlp=self.mlp3, group_all=True)
-        self.fc1 = nn.Linear(self.sa3.out_channel, self.fc1_out)
+            npoint=self.npoint3, radius=self.r3, nsample=self.nsample3, in_channel=3 + self.sa2.out_channel,
+            mlp=self.mlp3, radius_absolute=self.absolute_radius)
+        self.sa4 = PointNetSetAbstraction(
+            npoint=self.npoint4, radius=self.r4, nsample=self.nsample4, in_channel=3 + self.sa3.out_channel,
+            mlp=self.mlp4, radius_absolute=self.absolute_radius)
+        self.sa_last = PointNetSetAbstraction(
+            in_channel=3 + self.sa4.out_channel, mlp=self.mlp_last, group_all=True)
+        self.fc1 = nn.Linear(self.sa_last.out_channel, self.fc1_out)
         self.bn1 = nn.BatchNorm1d(self.fc1_out,
                                   momentum=0.1)
         self.drop1 = nn.Dropout(self.fc_dropout)
@@ -58,13 +76,15 @@ class get_model(nn.Module):
         l1_xyz, l1_points = self.sa1(in_xyz, in_points, max_std_per_probe)
         l2_xyz, l2_points = self.sa2(l1_xyz, l1_points, max_std_per_probe)
         l3_xyz, l3_points = self.sa3(l2_xyz, l2_points, max_std_per_probe)
-        x = l3_points.view(B, self.mlp3[-1])
+        l4_xyz, l4_points = self.sa4(l3_xyz, l3_points, max_std_per_probe)
+        l_final_xyz, l_final_points = self.sa_last(l4_xyz, l4_points, max_std_per_probe)
+        x = l_final_points.view(B, self.mlp_last[-1])
         x = self.drop1(F.relu(self.bn1(self.fc1(x))))
         x = self.drop2(F.relu(self.bn2(self.fc2(x))))
         x = self.fc3(x)
         x = F.log_softmax(x, -1)
 
-        return x, l3_points
+        return x, l_final_points
 
 
 class get_loss(nn.Module):
